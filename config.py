29342162cd6e96befe84134257a2ecbf65b4fc42
Niharika Mohapatra <niharikam2001@gmail.com>
2025-04-01 20:15:35 +0530

    Set database url

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+db_url = os.environ.get("DATABASE_URL", "").replace("postgres://", "postgresql://", 1)
+
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "you-will-never-guess")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1)
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    SQLALCHEMY_DATABASE_URI = db_url if db_url else f"sqlite:///{os.path.join(basedir, 'attention_test.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
