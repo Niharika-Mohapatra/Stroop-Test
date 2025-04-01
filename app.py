6a9a5276d449fa6bb1d688088126ecef1abeb323
Niharika Mohapatra <niharikam2001@gmail.com>
2025-04-01 23:03:21 +0530

    Exporting data to csv

--- app.py
+++ app.py
@@ -42,19 +42,19 @@ def submit():
     db.session.commit()
     return jsonify({"message": "Data saved!"}), 201
 
-results = StroopResult.query.all()
-
 @app.route("/data")
 def get_data():
+    results = StroopResult.query.all()
     return render_template("data.html", results=results)
 
 @app.route("/export_data")
 def export_data():
+    results = StroopResult.query.all()
     def generate():
         yield "word,color,response,reaction_time,is_correct\n"
         for result in results:
             yield f"{result.word},{result.color},{result.response},{result.reaction_time},{result.is_correct}\n"
-        return Response(generate(), mimetype="text/csv", headers={"Content-Disposition": "attachment; filename=stroop_results.csv"})
+    return Response(generate(), mimetype="text/csv", headers={"Content-Disposition": "attachment; filename=stroop_results.csv"})
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))  
